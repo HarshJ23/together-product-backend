350721c65190d2a71e3c34dad898eb337db323a2
HarshJ23 <hjawajiwar@gmail.com>
2024-01-27 19:42:02 +0530

    cors enabled

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /// api
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors"; 
 import 'dotenv/config';
 import { OpenAI, OpenAIEmbeddings } from "@langchain/openai";
 import { RetrievalQAChain } from "langchain/chains";
@@ -9,9 +10,10 @@ import { TokenTextSplitter } from "langchain/text_splitter";
 import { SearchApiLoader } from "langchain/document_loaders/web/searchapi";
 
 const app = express();
-const port = 3000; // You can change this to the desired port
+const port = 4000; // You can change this to the desired port
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // api keys
 // const openai_key = process.env['OPENAI_API_KEY']
@@ -29,7 +31,8 @@ app.post("/api/analyze", async (req, res) => {
       batchSize: 512,
     });
 
-    const { question, query } = req.body;
+    const {question,query} = req.body;
+    // const question = 'Give pros and cons by analyzing the specs and features of the mobile given in query';
 
     const loader = new SearchApiLoader({ q: query, apiKey, engine: "google" });
     const docs = await loader.load();
@@ -46,10 +49,12 @@ app.post("/api/analyze", async (req, res) => {
       embeddings
     );
 
+
     const chain = RetrievalQAChain.fromLLM(llm, vectorStore.asRetriever(), {
       verbose: true,
     });
 
+    
     const answer = await chain.call({ query: question });
 
     res.json({ answer: answer.text });
@@ -58,6 +63,8 @@ app.post("/api/analyze", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
